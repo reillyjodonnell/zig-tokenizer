db9216302ee172d5da54bab911b51b76156d5ad6
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2024-04-01 23:42:40 -0400

    updated how to read the file

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,23 @@
 const std = @import("std");
 const fs = @import("./file-reader.zig");
+const print = std.debug.print;
 
 pub fn main() !void {
-    try fs.fileReader("src/index.html");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var list = std.ArrayList(u8).init(allocator);
+    defer list.deinit();
+
+    const html_content = @embedFile("index.html");
+
+    for (html_content) |char| {
+        try list.append(char);
+    }
+
+    for (list.items) |item| {
+        print("{c}\n", .{item});
+    }
+
+    // try fs.fileReader("src/index.html");
 }
